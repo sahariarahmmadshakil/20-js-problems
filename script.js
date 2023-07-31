b719454445de4f3ac325dc10c9351fde9bf27a4e
sahariarahmmad <info.sashakil@gmail.com>
2023-08-01 02:44:37 +0600

    added three problems

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Js Output');
+const num1 = 10;
+const num2 = 20;
+const sum = num1 + num2;
+console.log(sum);
+
+// average of numbers 
+const input1 = 75.25;
+const input2 = 65.80;
+const input3 = 35.45;
+const input4 = 99.50;
+const average = (input1 + input2 + input3 + input4) / 4;
+console.log(average);
+//  reminder of number 
+const number = 128;
+const reminder = 128 % 5;
+console.log(reminder);
+const name = "jete jete pothe purnima rute chad utachilo gogone";
+const capName = name.toUpperCase();
+console.log(capName.toLowerCase());
