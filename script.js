a23c472944ca89a88a64d1f1dc5f6f43a83d4cc3
sahariarahmmad <info.sashakil@gmail.com>
2023-08-02 01:10:10 +0600

    array operation

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const num1 = 10;
-const num2 = 20;
-const sum = num1 + num2;
-console.log(sum);
+// const num1 = 10;
+// const num2 = 20;
+// const sum = num1 + num2;
+// console.log(sum);
 
-// average of numbers 
-const input1 = 75.25;
-const input2 = 65.80;
-const input3 = 35.45;
-const input4 = 99.50;
-const average = (input1 + input2 + input3 + input4) / 4;
-console.log(average);
-//  reminder of number 
-const number = 128;
-const reminder = 128 % 5;
-console.log(reminder);
-const name = "jete jete pothe purnima rute chad utachilo gogone";
-const capName = name.toUpperCase();
-console.log(capName.toLowerCase());
+// average of numbers
+// const input1 = 75.25;
+// const input2 = 65.80;
+// const input3 = 35.45;
+// const input4 = 99.50;
+// const average = (input1 + input2 + input3 + input4) / 4;
+// console.log(average);
+//  reminder of number
+// const number = 128;
+// const reminder = 128 % 5;
+// console.log(reminder);
+// const name = "jete jete pothe purnima rute chad utachilo gogone";
+// const capName = name.toUpperCase();
+// console.log(capName.toLowerCase());
+
+// declearation of an array
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers);
+// get element by index 
+const arrayIndex = numbers[3];
+console.log(arrayIndex);
+// get elements index 
+const elementIndex = numbers.indexOf(3);
+console.log(elementIndex);
+// set array element by index 
+numbers[4] = 35;
+console.log(numbers);
